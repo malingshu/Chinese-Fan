255ad760c7ccf732fc90b735bb9e8d45315138d7
Robert McCreary <rmccreary23@gmail.com>
2011-09-27 17:37:26 +0800

    added test.py

---
+++ tests.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unittest
+from name_tr_mod import UnihanDict
+
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        self.unihan = UnihanDict()
+        self.unihan.load()
+        self.unihan.set_pinyin_type(pinyin=1)
+    def test_pinyin(self):
+        hanzi = u"漢"
+        pinyin = self.unihan.convert(hanzi)
+        print hanzi + " : " + pinyin
+        self.assertEqual(pinyin, u"漢	(han4-tan1)")
+
+        
+if __name__ == '__main__':
+    unittest.main()
